a0bfeecd17d99be935ef31fc83e20eb9400d6717
shufengwu5946 <594695686@qq.com>
2019-06-28 00:13:48 +0800

    curd

--- curd/index.js
+++ curd/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 let app = express();
 
 app.engine("html", require("express-art-template"));
+
+app.use("/node_modules/", express.static("../node_modules/"));
+app.use("/public/", express.static("./public/"));
+
 app.listen(3000, () => {
   console.log("启动成功");
 });
