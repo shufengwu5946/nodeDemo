99777f21ef65ddd7d2476d48c035416e4bfa2067
shufeng.wu <shufeng.wu@deltaww.com>
2019-07-11 18:31:45 +0800

    mongoose

---
+++ mongoose/demo.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/test", { useNewUrlParser: true });
+
+const Cat = mongoose.model("Cat", { name: String });
+for (let i = 0; i < 10; i++) {
+  const kitty = new Cat({ name: "Zildjian" });
+  kitty.save().then(() => console.log("meow"));
+}
